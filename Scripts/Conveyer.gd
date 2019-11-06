557205a26a219ebcefcf5817dae61618ea8a20dd
Drone 7025 <lennartbladh69420@gmail.com>
2019-11-06 20:07:30 +0000

    Added music toggle button

--- Scripts/Conveyer.gd
+++ Scripts/Conveyer.gd
@@ -9,7 +9,6 @@ func _ready():
 	var foo = $Collision/Mesh.mesh.surface_get_material(1);
 	var bar = $Collision/Mesh.mesh.surface_get_material(1);
 	var heck = $Collision/Mesh.mesh.surface_get_material(2);
-	print(foo.get_property_list())
 	foo.flags_vertex_lighting = true;
 
 func _process(delta):
