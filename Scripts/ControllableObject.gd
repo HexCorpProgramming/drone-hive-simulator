92b8cf72f6a8b12b9457e06292e2cc2e76064aa3
Drone #1211 <raymond.i.fitzerik@is-a-furry.org>
2019-11-19 23:57:51 +0000

    Decoupled physics and controls from animation, for reusability.

---
+++ Scripts/ControllableObject.gd
@@ -0,0 +1,93 @@
+extends KinematicBody
+class_name ControllableObject
+
+var velocity = Vector3(0,0,0) #Velocity is speed in a given direction.
+var acceleration = Vector3(0,0,0) #Acceleration is the change in speed in a given direction.
+var jumpCounter = 0
+const SPEED = 6
+const MAX_SPEED = 4.8
+const GRAVITY = 981
+const JUMP_FORCE = GRAVITY/2
+const JUMP_LIMIT = 2
+
+
+func _ready():
+	jumpCounter = JUMP_LIMIT
+	print("Debug: ControllableObject _ready function complete.")
+
+
+func _physics_process(delta : float):
+	#Actual important functions that do stuff.
+	velocity = handle_inputs(delta)
+	handle_animation(velocity)
+	
+	print(jumpCounter)
+	print(velocity.x)
+	print(velocity.y)
+	print(velocity.z)
+	
+	move_and_slide(velocity) #Applies a given Vector3 to an object.
+
+
+func handle_animation(velocity : Vector3):
+	#Interface function
+	return
+
+
+func handle_inputs(delta : float):
+	jump()
+	velocity.z = update_velocity_axis(velocity.z,"ui_up","ui_down")
+	velocity.x = update_velocity_axis(velocity.x,"ui_left","ui_right")
+	return process_movement(delta, velocity)
+
+
+func jump():
+	if Input.is_action_just_pressed("Jump") and jumpCounter > 0:
+		acceleration.y = -JUMP_FORCE
+		jumpCounter -= 1
+	pass
+
+
+func update_velocity_axis(velocityAxis : float, positiveMovementCommand : String, negativeMovementCommand : String):
+	if Input.is_action_pressed(positiveMovementCommand):
+		velocityAxis = -SPEED
+	if Input.is_action_pressed(negativeMovementCommand):
+		velocityAxis = SPEED
+	if Input.is_action_pressed(positiveMovementCommand) and Input.is_action_pressed(negativeMovementCommand):
+		velocityAxis = 0
+		#If positiveMovementCommand and negativeMovementCommand are pressed at the same time, the drone won't move in either direction.
+	else:
+		velocityAxis = lerp(velocityAxis,0,0.2)
+	
+	return velocityAxis
+
+
+func process_movement(delta : float, velocity : Vector3):
+	velocity = process_jump(delta, velocity)
+	velocity = limit_speed(velocity)
+	return velocity
+
+
+func process_jump(delta : float, velocity : Vector3):
+	acceleration.y += GRAVITY * delta
+	velocity.y = -acceleration.y * delta
+	
+	# TODO: Should be floor
+	if is_on_wall():
+		jumpCounter = JUMP_LIMIT
+		velocity.y = 0
+		
+	return velocity
+
+
+func limit_speed(velocity : Vector3):
+	if (abs(velocity.x) + abs(velocity.z)) > MAX_SPEED+0.2:
+		velocity.y = 0 
+		# ^ Something buggy is happening with the Y axis and needs resetting for the calculation
+		# This also breaks jumping unless the vertical MAX_SPEED is slightly higher than horizontal MAX_SPEED
+		# Also, if you press an X and Z movement key just once the drone keeps moving, not sure if related
+		# Turns out if the drone jumps and does the above it stays at that height without falling
+		var direction = velocity.normalized()
+		velocity = direction * MAX_SPEED
+		
+	return velocity
--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -1,33 +1,20 @@
-extends KinematicBody
+extends ControllableObject
 
-var velocity = Vector3(0,0,0) #Velocity is speed in a given direction.
-var acceleration = Vector3(0,0,0) #Acceleration is the change in speed in a given direction.
-var jumpCounter = 0
-const SPEED = 6
-const MAX_SPEED = 4.8
-const GRAVITY = 981
-const JUMP_FORCE = GRAVITY/2
-const JUMP_LIMIT = 2
 export var droneID = "6"
 
 
+#Override
 func _ready():
+	._ready()
 	$Body.playing = false
 	$Body.frame = 0
 	get_node("Face").assign_id(droneID)
-	jumpCounter = JUMP_LIMIT
 	print("Debug: Player drone _ready function complete.")
 
 
+#Override
 func _physics_process(delta : float):
-	#Actual important functions that do stuff.
-	handle_inputs(delta)
-	
-	print(jumpCounter)
-	print(velocity.x)
-	print(velocity.y)
-	print(velocity.z)
-	print($Body.frame)
+	._physics_process(delta)
 	
 	#I just thought it'd be funny to have an "obey" function called *constantly*. These don't actually do anything.
 	#It's thematically appropriate!
@@ -35,57 +22,9 @@ func _physics_process(delta : float):
 	good_drone()
 
 
-func handle_inputs(delta : float):
-	set_animation(velocity)
-	jump()
-	velocity.z = update_velocity_axis(velocity.z,"ui_up","ui_down")
-	velocity.x = update_velocity_axis(velocity.x,"ui_left","ui_right")
-	velocity = process_movement(delta, velocity)
-	move_and_slide(velocity) #Applies a given vector3 to an object.
-
-
-func jump():
-	if Input.is_action_just_pressed("Jump") and jumpCounter > 0:
-		acceleration.y = -JUMP_FORCE
-		jumpCounter -= 1
-	pass
-
-
-func process_movement(delta : float, velocity : Vector3):
-
-	velocity = process_jump(delta, velocity)
-	velocity = limit_speed(velocity)
-	
-	return velocity
-	
-
-
-func process_jump(delta : float, velocity : Vector3):
-	acceleration.y += GRAVITY * delta
-	velocity.y = -acceleration.y * delta
-	
-	# TODO: Should be floor
-	if is_on_wall():
-		jumpCounter = JUMP_LIMIT
-		velocity.y = 0
-		
-	return velocity
-
-
-func limit_speed(velocity : Vector3):
-	if (abs(velocity.x) + abs(velocity.z)) > MAX_SPEED+0.2:
-		velocity.y = 0 
-		# ^ Something buggy is happening with the Y axis and needs resetting for the calculation
-		# This also breaks jumping unless the vertical MAX_SPEED is slightly higher than horizontal MAX_SPEED
-		# Also, if you press an X and Z movement key just once the drone keeps moving, not sure if related
-		# Turns out if the drone jumps and does the above it stays at that height without falling
-		var direction = velocity.normalized()
-		velocity = direction * MAX_SPEED
-		
-	return velocity
-
-
-func set_animation(velocity : Vector3):
+#Override
+func handle_animation(velocity : Vector3):
+	.handle_animation(velocity)
 	toggle_animation_orientation(velocity)
 	toggle_animation_play(velocity)
 
@@ -110,20 +49,6 @@ func toggle_animation_orientation(velocity : Vector3):
 		$Body.rotation_degrees.y = 180 
 		$Face.translation = Vector3(-0.4, 2, 0.1)
 		$Face/ID.translation = Vector3(0.099, 0, 0)
-
-
-func update_velocity_axis(velocityAxis : float, positiveMovementCommand : String, negativeMovementCommand : String):
-	if Input.is_action_pressed(positiveMovementCommand):
-		velocityAxis = -SPEED
-	if Input.is_action_pressed(negativeMovementCommand):
-		velocityAxis = SPEED
-	if Input.is_action_pressed(positiveMovementCommand) and Input.is_action_pressed(negativeMovementCommand):
-		velocityAxis = 0
-		#If positiveMovementCommand and negativeMovementCommand are pressed at the same time, the drone won't move in either direction.
-	else:
-		velocityAxis = lerp(velocityAxis,0,0.2)
-	
-	return velocityAxis
 
 
 func obey():
