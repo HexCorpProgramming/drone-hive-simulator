f5ad69183e72edaacc370b0dd80ca5149588dec1
HexDrone1211 <hexdrone1211@gmail.com>
2019-11-22 20:31:02 +0000

    "Rewrote movement with direction changes, rather than speed/velocity."

--- Scripts/ControllableObject.gd
+++ Scripts/ControllableObject.gd
@@ -6,10 +6,11 @@ var acceleration = Vector3(0,0,0) #Acceleration is the change in speed in a give
 var jumpCounter = 0
 const SPEED = 6
 const MAX_SPEED = 4.8
-const GRAVITY = 981
-const JUMP_FORCE = GRAVITY/2
+const GRAVITY = 800
+const JUMP_FORCE = GRAVITY/5
 const JUMP_LIMIT = 2
 
+onready var ground_ray = get_node("GroundRay")
 
 func _ready():
 	jumpCounter = JUMP_LIMIT
@@ -17,13 +18,14 @@ func _ready():
 
 
 func _physics_process(delta : float):
-	velocity = handle_inputs(delta)
+	
+	velocity = generate_velocity(generate_direction(), delta)
+	
 	handle_animation(velocity)
 	
-	print(jumpCounter)
-	print(velocity.x)
-	print(velocity.y)
-	print(velocity.z)
+	print(velocity)
+	print(velocity.normalized())
+	#print(acceleration)
 	
 	move_and_slide(velocity) #Applies a given Vector3 to an object.
 
@@ -33,55 +35,40 @@ func handle_animation(velocity : Vector3):
 	return
 
 
-func handle_inputs(delta : float):
-	jump()
-	velocity.z = update_velocity_axis(velocity.z,"ui_up","ui_down")
-	velocity.x = update_velocity_axis(velocity.x,"ui_left","ui_right")
-	return process_movement(delta, velocity)
-
-
-func jump():
-	if Input.is_action_just_pressed("Jump") and jumpCounter > 0:
-		acceleration.y = -JUMP_FORCE
-		jumpCounter -= 1
-	pass
-
-
-func update_velocity_axis(velocityAxis : float, positiveMovementCommand : String, negativeMovementCommand : String):
-	if Input.is_action_pressed(positiveMovementCommand):
-		velocityAxis = -SPEED
-	if Input.is_action_pressed(negativeMovementCommand):
-		velocityAxis = SPEED
-	if Input.is_action_pressed(positiveMovementCommand) and Input.is_action_pressed(negativeMovementCommand):
-		velocityAxis = 0
-		#If positiveMovementCommand and negativeMovementCommand are pressed at the same time, the drone won't move in either direction.
-	else:
-		velocityAxis = lerp(velocityAxis,0,0.2)
+func generate_direction():
+	var direction = Vector3(0,0,0) #Direction of movement
 	
-	return velocityAxis
-
-
-func process_movement(delta : float, velocity : Vector3):
-	velocity = process_jump(delta, velocity)
-	velocity = limit_speed(velocity)
-	return velocity
-
-
-func process_jump(delta : float, velocity : Vector3):
-	acceleration.y += GRAVITY * delta
-	velocity.y = -acceleration.y * delta
+	var go_north = Input.is_action_pressed("ui_up")
+	var go_south = Input.is_action_pressed("ui_down")
+	var go_west = Input.is_action_pressed("ui_left")
+	var go_east = Input.is_action_pressed("ui_right")
 	
-	# TODO: Should be floor
-	if is_on_wall():
-		jumpCounter = JUMP_LIMIT
-		velocity.y = 0
+	if go_north:
+		direction.z += -1
+	elif go_south:
+		direction.z += 1
+	elif go_north and go_south:
+		direction.z = 0
 		
-	return velocity
+	if go_west:
+		direction.x += -1
+	elif go_east:
+		direction.x += 1
+	if go_west and go_east:
+		direction.x = 0
+		
+	return direction.normalized()
 
 
-func limit_speed(velocity : Vector3):
-	if (abs(velocity.x) + abs(velocity.z)) > MAX_SPEED:
-		var direction = velocity.normalized()
+func generate_velocity(direction : Vector3, delta : float):
+	var velocity = Vector3(0,0,0)
+	
+	if direction.is_normalized():
 		velocity = direction * MAX_SPEED
+			
+	if Input.is_action_just_pressed("Jump") and ground_ray.is_colliding():
+		velocity.y = JUMP_FORCE
 		
+	velocity.y -= delta * GRAVITY			
+			
 	return velocity
