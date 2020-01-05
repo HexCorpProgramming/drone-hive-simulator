d70110995af65c4fc7663ed3b0c2514c309176c7
HexCorpDrone <hexcorpdrone@gmail.com>
2020-01-05 16:12:53 +0000

    Changed script files to be nonunique

--- Scripts/Conveyer.gd
+++ Scripts/Conveyer.gd
@@ -5,11 +5,8 @@ onready var thisMesh = get_node("Collision/Mesh")
 onready var thisMat = thisMesh.mesh.surface_get_material(1)
 
 func _ready():
+	thisMat.set_local_to_scene(true)
 	thisMesh.mesh.surface_set_material(1, thisMat.duplicate())
-	print(thisMat)
-	print(thisMesh.mesh.surface_get_material(1))
-	
-	print(thisMesh.mesh.surface_get_material(1).uv1_offset)
 
 func _physics_process(delta):
 	thisMesh.mesh.surface_get_material(1).uv1_offset.y += self.speed
