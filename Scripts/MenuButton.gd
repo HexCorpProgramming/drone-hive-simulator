bdc669a33a5ee91d58d75bec9d257760cc45b464
1211 <hexdrone1211@gmail.com>
2020-01-05 17:53:08 +0000

    Added settings menu for drone ID.

--- Scripts/MenuButton.gd
+++ Scripts/MenuButton.gd
@@ -1,16 +1,4 @@
 extends Button
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-
-
 func _on_Button_pressed():
-	print("button pressed")
-	get_tree().change_scene("res://Levels/LevelBox.tscn")
+	get_tree().change_scene("res://Levels/SettingsMenu.tscn")
---
+++ Scripts/SettingsButton.gd
@@ -0,0 +1,10 @@
+extends Button
+
+onready var dronename = get_node("../NameBox")
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://Levels/LevelBox.tscn")
+	if !dronename.text.empty():
+		PlayerSettings.change_id(dronename.text)
+	else:
+		PlayerSettings.change_id(PlayerSettings.randomize_id())
