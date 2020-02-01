9038b30f433dbadc016298db56a404fab30b3871
HexCorpDrone <hexcorpdrone@gmail.com>
2020-02-01 11:21:48 +0000

    Added toggleability(sic) to InteractableObject

--- Scripts/Abstract/InteractableObject.gd
+++ Scripts/Abstract/InteractableObject.gd
@@ -2,7 +2,29 @@ extends Spatial
 class_name InteractableObject
 
 var currentUser = null
-var toggleable = true
+var isToggleable = false
+var isToggled = false
+
+func _ready():
+	isToggled = false
 
 func interact(interacter):
 	print("interacting with ", interacter)
+	if isToggleable:
+		isToggled = !isToggled
+		if isToggled:
+			toggle_on(interacter)
+		else:
+			toggle_off(interacter)
+	else:
+		toggle_on(interacter)
+	
+func toggle_on(interacter):
+	print("Toggled on by ", interacter)
+	currentUser = interacter
+
+func toggle_off(interacter):
+	print("Toggled off by ", interacter)
+	currentUser = null
+	
+	
