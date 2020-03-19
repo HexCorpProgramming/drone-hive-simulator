aa3eb03591f819ae804f33ffa49c9a52d8d1ecba
HexCorpDrone <hexcorpdrone@gmail.com>
2020-03-19 19:38:01 +0000

    Added more functionality to clickselect drop target

--- Objects/UI/ClickSelect/ClickSelectUI.gd
+++ Objects/UI/ClickSelect/ClickSelectUI.gd
@@ -3,6 +3,8 @@ extends Control
 var picked_item = null
 var item_preview = null
 
+onready var PlayerDrone = load("res://Objects/Drones/PlayerDrone/PlayerDrone.tscn")
+
 var vertical_offset = Vector3(0,7,0)
 var last_known_location = Vector3(0,50,0)
 
@@ -13,6 +15,7 @@ func add_button():
 	$PanelContainer/ScrollContainer/HBoxContainer.add_child(button)
 	
 func set_item(item):
+	$DropTarget.visible = false
 	if item_preview:
 		item_preview.queue_free()
 		item_preview = null
@@ -29,16 +32,17 @@ func _ready():
 	visible = false
 	
 func _process(delta):
-	if picked_item: #If we have an item ready to place, we should have a copy of it follow the mouse.
-		var result = raycast_from_camera_to_mouse()
-		if result:
-			item_preview.visible = true
-			item_preview.translation = lerp(item_preview.translation, result.collider.translation + vertical_offset, 0.1)
+	if picked_item:
+		handle_drop_target()
+		handle_item_preview()
+	
 		
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed and picked_item:
 		print("Mouse clicked!")
-		var found_floor = $WorldGetter.get_world().direct_space_state.intersect_ray(item_preview.translation, item_preview.translation - Vector3(0,50,0))
+		var found_floor = raycast_from_object_to_ground(item_preview)
+		if found_floor:
+			print(found_floor.collider)
 		if found_floor and found_floor.collider.is_in_group("Floor"): 
 			print("floor found.")
 			var spawned_item = picked_item.instance()
@@ -48,6 +52,28 @@ func _input(event):
 	if Input.is_action_just_pressed("clickdrop_cancel_item"):
 		set_item(null)
 		
+func handle_drop_target():
+	var drop_target_location = raycast_from_object_to_ground(item_preview)
+	if drop_target_location:
+		$DropTarget.translation = drop_target_location.position + Vector3(0,0.1,0)
+		if drop_target_location.collider.is_in_group("Floor"):
+			$DropTarget.valid = true
+		elif drop_target_location.collider.is_in_group("Drone"): #We need a special case for the drone.
+			var avoid_this_drone = drop_target_location.collider
+			$DropTarget.visible = true
+			$DropTarget.valid = false
+			$DropTarget.translation = raycast_from_object_to_ground(item_preview, avoid_this_drone).position + Vector3(0,0.1,0) #i hate this code t. 5890
+		else:
+			$DropTarget.valid = false
+	else:
+		$DropTarget.visible = false
+		
+func handle_item_preview():
+	var result = raycast_from_camera_to_mouse()
+	if result:
+		$DropTarget.visible = true
+		item_preview.visible = true
+		item_preview.translation = lerp(item_preview.translation, result.collider.translation + vertical_offset, 0.1)
 		
 func raycast_from_camera_to_mouse():
 	var mouse_position = get_viewport().get_mouse_position()
@@ -55,3 +81,6 @@ func raycast_from_camera_to_mouse():
 	var normal = get_viewport().get_camera().project_ray_normal(mouse_position)
 	var to = from + normal * 100
 	return $WorldGetter.get_world().direct_space_state.intersect_ray(from, to, [item_preview])
+	
+func raycast_from_object_to_ground(object, avoid = null):
+	 return $WorldGetter.get_world().direct_space_state.intersect_ray(item_preview.translation, item_preview.translation - Vector3(0,50,0), [avoid], 1)
