364768eb0918d12158bb0dd536011508c80af191
HexCorpDrone <hexcorpdrone@gmail.com>
2020-04-21 20:01:51 +0100

    FloorSpawner much more modular, lots of helper functions

--- Objects/Scene/FloorSpawner.gd
+++ Objects/Scene/FloorSpawner.gd
@@ -25,10 +25,10 @@ func create_default_tiles():
 	#Reset the tile array
 	tiles.clear()
 	
+	#Add the tiles
 	add_rows(default_x, default_y)
-	add_tiles(Vector2(0,0), Vector2(3,3))
-
-	
+	add_tiles(0,0,15,15)
+	delete_tiles(1,1,14,14)
 	get_all_tiles()
 	
 func get_tile(x,y):
@@ -37,8 +37,19 @@ func get_tile(x,y):
 		return null
 	return tiles[x][y]
 	
+func get_all_tiles():
+	var flattened_list = []
+	for row in tiles:
+		for tile in row:
+			if tile != null:
+				flattened_list.append(tile)
+	print("TOTAL COUNT OF TILES: ",flattened_list.size())
+	return flattened_list
+	
 func add_tile(x,y):
 	if !valid(x,y):
+		return
+	if get_tile(x,y) != null:
 		return
 	var new_tile = tile_source.instance()
 	new_tile.name = str(x)+","+str(y)
@@ -46,6 +57,46 @@ func add_tile(x,y):
 	new_tile.translation = Vector3(new_tile.scale.x * 2 * x, 0, new_tile.scale.z * 2 * y)
 	add_child(new_tile)
 	tiles[x][y] = new_tile
+	
+func add_tiles(from_x, from_y, to_x, to_y):
+	if !valid(from_x, from_y):
+		print("beep")
+		return
+	if !valid(to_x, to_y):
+		print("boop")
+		return
+	if (from_x == to_x and from_y == to_y):
+		print("add_tiles parameters cannot be identical.")
+		return
+	for y in range(from_y, to_y):
+		print("beep")
+		for x in range(from_x, to_x):
+			print("boop")
+			add_tile(x,y)
+	
+func delete_tile(x,y):
+	if !valid(x,y):
+		return
+	var tile = get_tile(x,y)
+	if tile != null and tile.has_method("queue_free"):
+		tile.queue_free()
+		tiles[x][y] = null
+	
+func delete_tiles(from_x, from_y, to_x, to_y):
+	if !valid(from_x, from_y):
+		print("beep")
+		return
+	if !valid(to_x, to_y):
+		print("boop")
+		return
+	if (from_x == to_x and from_y == to_y):
+		print("add_tiles parameters cannot be identical.")
+		return
+	for y in range(from_y, to_y):
+		print("beep")
+		for x in range(from_x, to_x):
+			print("boop")
+			delete_tile(x,y)
 	
 func add_rows(size, rows = 1):
 	#Size and rows will be +1 as a gutter so we don't go out of bounds
@@ -59,51 +110,6 @@ func add_rows(size, rows = 1):
 			row.append(null)
 		tiles.append(row)
 	print("Added ", rows+1, " row(s).")
-	
-func delete_tile(x,y):
-	if !valid(x,y):
-		return
-	var tile = get_tile(x,y)
-	if tile != null and tile.has_method("queue_free"):
-		tile.queue_free()
-		tiles[x][y] = null
-	
-func delete_tiles(to, from):
-	if !valid(from.x, from.y):
-		return
-	if !valid(to.x, to.y):
-		return
-	if (from.x == to.x and from.y == to.y):
-		print("delete_tiles parameters cannot be identical.")
-		return
-	for y in range(from.y, to.y):
-		for x in range(from.x, to.x):
-			delete_tile(x,y)
-	
-func add_tiles(to, from):
-	if !valid(from.x, from.y):
-		print("beep")
-		return
-	if !valid(to.x, to.y):
-		print("boop")
-		return
-	if (from.x == to.x and from.y == to.y):
-		print("add_tiles parameters cannot be identical.")
-		return
-	for y in range(from.y, to.y):
-		print("beep")
-		for x in range(from.x, to.x):
-			print("boop")
-			add_tile(x,y)
-
-func get_all_tiles():
-	var flattened_list = []
-	for row in tiles:
-		for tile in row:
-			if tile != null:
-				flattened_list.append(tile)
-	print("TOTAL COUNT OF TILES: ",flattened_list.size())
-	return flattened_list
 	
 func valid(x,y):
 	return x <= tiles.size() and y <= tiles[x].size()
