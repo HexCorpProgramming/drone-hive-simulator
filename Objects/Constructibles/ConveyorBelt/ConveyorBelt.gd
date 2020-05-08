5b6fa536359acd86c0954db9c78a80010338641a
HexCorpDrone <hexcorpdrone@gmail.com>
2020-05-08 18:34:54 +0100

    Added tick system, made conveyor belts tickable

--- Objects/Constructibles/ConveyorBelt/ConveyorBelt.gd
+++ Objects/Constructibles/ConveyorBelt/ConveyorBelt.gd
@@ -1,4 +1,19 @@
 extends Spatial
 
+var move = false
+
+func _ready():
+	$Timer.connect("timeout",self,"stop_moving")
+
 func _process(delta):
-	$Body/Collision/Mesh.get_surface_material(1).uv1_offset.y += 0.01
+	if move:
+		$Body/Collision/Mesh.get_surface_material(1).uv1_offset.y += 0.01
+	
+func tick():
+	move = true
+	print("This conveyor belt is being ticked.")
+	$Timer.start(1)
+	
+func stop_moving():
+	move = false
+	
