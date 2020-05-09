20f264c3f9426372ee79bb22adadbb80698e59cb
HexCorpDrone <hexcorpdrone@gmail.com>
2020-05-09 14:43:07 +0100

    proper randomization for subjectnames

--- Scripts/SubjectNames.gd
+++ Scripts/SubjectNames.gd
@@ -3,4 +3,5 @@ extends Node
 var names = ["Hex","Remi","Sonic the Hedgehog","Aiden Claus","May Den","Garfield","Jon Arbuckle"]
 
 func get_name():
+	randomize()
 	return names[randi() % names.size()]
