e17c304d8e8ae233bb14fbef14557cc626a0bf4a
HexCorpDrone <hexcorpdrone@gmail.com>
2020-05-11 19:30:41 +0100

    You can now click drones to view their stats

--- Levels/BoxUIScript.gd
+++ Levels/BoxUIScript.gd
@@ -1,8 +1,16 @@
 extends Control
 
-func update_stat_text(personable, hardworking, creative, recruit_name):
+func update_stat_text_recruit(personable, hardworking, creative, recruit_name):
 	$StatLabel.visible = true
 	$StatLabel.text = "Name: " + str(recruit_name) + ". \n"
 	$StatLabel.text += "Personable: " + str(personable) + "\n"
 	$StatLabel.text += "Hardworking: " + str(hardworking) + "\n"
 	$StatLabel.text += "Creativity: " + str(creative) + "\n"
+	
+func update_stat_text_drone(emote, productivity, innovation, charge, drone_id):
+	$StatLabel.visible = true
+	$StatLabel.text = "Drone ID: " + str(drone_id) + ". \n"
+	$StatLabel.text += "Productivity: " + str(productivity) + "\n"
+	$StatLabel.text += "Emotional intelligence: " + str(emote) + "\n"
+	$StatLabel.text += "innovation: " + str(innovation) + "\n"
+	$StatLabel.text += "Charge level: " + str(charge) + "%\n"
