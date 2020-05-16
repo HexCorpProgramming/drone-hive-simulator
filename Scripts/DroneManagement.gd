7275262ba96f7f5a5163381b11651d21cc0454b9
HexCorpDrone <hexcorpdrone@gmail.com>
2020-05-16 16:32:34 +0100

    added funcywunks for the dms

--- Scripts/DroneManagement.gd
+++ Scripts/DroneManagement.gd
@@ -15,18 +15,36 @@ func register_new_drone(drone):
 	print("Not implemented.")
 
 func register_expedition_drone(drone):
-	print("Not implemented.")
+	for exp_drone in expedition_drones:
+		if exp_drone.drone_id == drone.drone_id:
+			print("Given drone is already on an expedition.")
+			return
+	
+	var drone_stats = DroneStats.new()
+	drone_stats.drone_id = drone.drone_id
+	drone_stats.emotional_intelligence = drone.emotional_intelligence
+	drone_stats.productivity = drone.productivity
+	drone_stats.innovative = drone.innovative
+	drone_stats.charge = drone.charge
 
-func retrieve_expedition_drone(drone_id):
-	#Delete drone from list and return its details.
-	print("Not implemented.")
+func retrieve_expedition_drone(drone_id) -> DroneStats:
+	for drone in expedition_drones:
+		if drone.drone_id == drone_id:
+			print("Drone found on expedition.")
+			stored_drones.erase(drone)
+			return drone
+	return null
 	
 func register_stored_drone(drone):
 	print("Not implemented.")
 	
 func retrieve_stored_drone(drone_id):
-	#Delete drone from list and return its details.
-	print("Not implemented.")
+	for drone in stored_drones:
+		if drone.drone_id == drone_id:
+			print("Drone found in storage.")
+			stored_drones.erase(drone)
+			return drone
+	return null
 
 func _get_stats_from_drone(drone):
 	#Drone must be a node path.
