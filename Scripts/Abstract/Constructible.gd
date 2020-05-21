496f4a5bb68bdb8fab045aeb0debea890a13af3b
HexCorpDrone <hexcorpdrone@gmail.com>
2020-05-21 18:14:35 +0100

    Better wording IMO

--- Scripts/Abstract/Constructible.gd
+++ Scripts/Abstract/Constructible.gd
@@ -3,8 +3,8 @@ class_name Constructible
 
 var money_cost = 0
 var nanite_cost = 0
-var current_tick = 0
-var use_time = 0 #Use this later for setting how many ticks it takes to do something.
+var tick_progression = 0
+var completion_tick = 0 #Use this later for setting how many ticks it takes to do something.
 
 func tick():
 	print("Tick!")
