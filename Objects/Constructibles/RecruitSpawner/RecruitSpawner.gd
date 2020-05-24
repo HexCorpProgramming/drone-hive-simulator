a63587bd72113541052a92fe0731d8aad8ac73b0
HexCorpDrone <hexcorpdrone@gmail.com>
2020-05-24 13:04:18 +0100

    Toggle logic for recruit spawner implemented

--- Objects/Constructibles/RecruitSpawner/RecruitSpawner.gd
+++ Objects/Constructibles/RecruitSpawner/RecruitSpawner.gd
@@ -3,6 +3,7 @@ extends Spatial
 var delay = 5
 var tick = 0
 var subject_source = load("res://Objects/Recruit.tscn")
+var spawn_recruits = true
 
 func _ready():
 	print("Spawner spawned. Ready to spawn.")
@@ -10,11 +11,15 @@ func _ready():
 	
 func tick():
 	tick += 1
-	if tick == delay:
+	if tick == delay and spawn_recruits:
 		var new_subject = subject_source.instance()
 		new_subject.translation = get_global_transform().origin
 		get_tree().get_root().add_child(new_subject)
+	
+	if tick == delay:
 		tick = 0
 		
-func toggle_spawn(event):
-	if event 
+func toggle_spawn(camera, event, click_pos, click_norm, shape_idx):
+	if Input.is_action_just_pressed("click"):
+		spawn_recruits = !spawn_recruits
+		get_node("../SpotLight").light_color = Color(1,0.4,1,0) if spawn_recruits else Color(1,0.4,0.4,0)
